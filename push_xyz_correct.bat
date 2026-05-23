04e8188ff4a7acb86f0d04327de5f202f4fb36e2
CH刘龙解 <15514859+ch-liu-longjie@user.noreply.gitee.com>
2026-05-23 20:49:44 +0800

    Initial commit - Fix API parameters

---
+++ push_xyz_correct.bat
@@ -0,0 +1,9 @@
+cd /d "d:\临时\代码重构工程\longjieruankong.xyz"
+git init
+git remote remove origin
+git remote add origin https://github.com/CHliulongjie/longjieruankong.xyz.git
+git add .
+git commit -m "Initial commit - Fix API parameters"
+git branch -M main
+git push -u origin main --force
+pause
